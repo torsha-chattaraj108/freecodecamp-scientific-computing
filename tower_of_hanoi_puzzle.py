e231959a3cb3c3771bea7cbdd7b778646369d172
Torsha Chattaraj <chattarajtorsha18@gmail.com>
2026-07-10 21:30:05 +0530

    Print pegs in one line for better readability

--- tower_of_hanoi_puzzle.py
+++ tower_of_hanoi_puzzle.py
@@ -7,6 +7,6 @@ def move(n,source,auxiliary,target):
         return
     move(n-1,source,target,auxiliary)#move n-1 disks from source to auxiliary so they are out of the way
     target.append(source.pop())#now move the nth disk that we left in source to target
-    print(A,B,C,'\n')#print our progress
+    print(f"A: {A} | B: {B} | C: {C}")#print our progress
     move(n-1,auxiliary,source,target)#move the n-1 disks from auxiliary to target
 move(NUMBER_OF_DISKS,A,B,C)#initiate call from source A to target C with auxiliary C
