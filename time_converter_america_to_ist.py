bcebdbb2267f4f2b423b68f4539cdb1dd78e1ecc
Torsha Chattaraj <chattarajtorsha18@gmail.com>
2026-07-18 19:34:19 +0530

    Add time converter script for converting US time zones to IST

---
+++ time_converter_america_to_ist.py
@@ -0,0 +1,56 @@
+def time_converter(start,us_tz,day_of_week=None,is_dst=False):
+    tz_list={'ET':10.5,
+             'CT':11.5,
+             'MT':12.5,
+             'PT':13.5,
+             'AST':9.5
+             }
+    week=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    s_time,am_pm=start.split(' ')
+    s_hour,s_min=s_time.split(':')
+    offset_value=tz_list[us_tz]
+    if is_dst:
+        offset_value-=1.0
+    u_hour=int(offset_value)
+    u_min=round((offset_value%1)*60)
+    start_time=int(s_hour)
+    if am_pm=='PM' and start_time!=12:
+        start_time+=12
+    elif am_pm=='AM' and start_time==12:
+        start_time=0
+    t_hour=start_time+u_hour
+    t_min=int(s_min)+u_min
+    if t_min>=60:
+        hours=t_min//60
+        t_min=t_min%60
+        t_hour+=hours
+    f_hour=t_hour%24
+    if f_hour>=12:
+        am_pm='PM'
+    else:
+        am_pm='AM'
+    days=t_hour//24
+    if f_hour==0:
+        f_hour_12=12
+    elif f_hour>12:
+        f_hour_12=f_hour-12
+    elif f_hour==12:
+        f_hour_12=12
+    else:
+        f_hour_12=f_hour
+    f_min=f'{t_min:02d}'
+    days=t_hour//24
+    day_string=''
+    if days==1:
+        day_string=f' (next day)'
+    elif days>1:
+        day_string=f' ({days} days later)'
+    if day_of_week!=None:
+        start_index=week.index(day_of_week)
+        new_index=(start_index+days)%7
+        f_time=f'{f_hour_12}:{f_min} {am_pm} {week[new_index]}'
+    else:
+        f_time=f'{f_hour_12}:{f_min} {am_pm}{day_string}'
+    return f_time
+
+print(time_converter("3:55 PM", "ET", "Sunday", is_dst=True))
